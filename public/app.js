6182e30b9fd6916da411ba6dc60d8dfdb57f5c40
FlaconVerde <Laupa88@hotmail.com.ar>
2021-06-23 14:19:32 -0300

    1st functional upload

---
+++ public/app.js
@@ -0,0 +1,59 @@
+const pusher = new Pusher('d28760b60173a3e99d03', {
+    cluster: 'us2',
+    encrypted: true,
+    authEndpoint: 'pusher/auth'
+});
+
+const app = new Vue({
+    el: '#app',
+    data: {
+        joined: false,
+        username: '',
+        members: '',
+        newMessage: '',
+        messages: [],
+        time: '',
+        status: ''
+    },
+    methods: {
+        joinChat() {
+            axios.post('join-chat', {username: this.username})
+                .then(response => {
+                    // User has joined the chat
+                    this.joined = true;
+                    const channel = pusher.subscribe('Chat');
+                    channel.bind('pusher:subscription_succeeded', (members) => {
+                        this.members = channel.members;
+                    });
+                    // Listen for chat messages
+                    this.listen();
+                });
+        },
+        sendMessage() {
+            var today = new Date();
+            var h = today.getHours();
+            var mi = today.getMinutes();
+            var time = (h < 10 ? '0' + h : h) + ':' + (mi < 10 ? '0' + mi : mi);
+            var Time = time.toString();
+            let message = {
+                username: this.username,
+                time: Time,
+                message: this.newMessage
+            }
+            // Clear input field
+            this.newMessage = '';
+            axios.post('/send-message', message);
+        },
+        listen() {
+            const channel = pusher.subscribe('Chat');
+            channel.bind('message_sent', (data) => {
+                this.messages.push({
+                    username: data.username,
+                    time: data.time,
+                    message: data.message
+                });
+            });
+        }
+    }
+});
+
---
+++ server.js
@@ -0,0 +1,68 @@
+// server.js
+    require('dotenv').config();
+    const express    = require('express');
+    const path       = require('path');
+    const bodyParser = require('body-parser');
+    const session    = require('express-session');
+    const Pusher     = require('pusher');
+
+    const app = express();
+
+    // Session middleware
+    app.use(session({
+        secret: 'somesuperdupersecret',
+        resave: true,
+        saveUninitialized: true
+    }))
+
+    // Body parser middleware
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: false }));
+
+    //static files
+    app.use(express.static(path.join(__dirname, 'public')));
+
+    // Create an instance of Pusher
+    const pusher = new Pusher({
+        appId: "1221976",
+        key: "d28760b60173a3e99d03",
+        secret: "171eff65e3245b57da9d",
+        cluster: "us2",
+        useTLS: true
+    });
+
+    app.listen(3000, () => {
+        console.log('Server is up on 3000')
+    });
+
+    // server.js
+
+    app.get('/', (req, res) => {
+        res.sendFile('index.html');
+    });
+
+    app.post('/join-chat', (req, res) => {
+        // store username in session
+        req.session.username = req.body.username;
+        res.json('Joined');
+    });
+
+    app.post('/pusher/auth', (req, res) => {
+        const socketId = req.body.socket_id;
+        const channel = req.body.channel_name;
+        // Retrieve username from session and use as presence channel user_id
+        const presenceData = {
+            user_id: req.session.username
+        };
+        const auth = pusher.authenticate(socketId, channel, presenceData);
+        res.send(auth);
+    });
+
+    app.post('/send-message', (req, res) => {
+        pusher.trigger('Chat', 'message_sent', {
+            username: req.body.username,
+            time: req.body.time,
+            message:  req.body.message
+        });
+        res.send('Message sent');
+    });
